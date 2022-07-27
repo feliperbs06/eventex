82fca9fef8cbd4aed7256a5fbf9a489659fd478c
Felipe Souza <feliperbs06@gmail.com>
2022-07-27 11:26:49 -0300

    Import project

---
+++ eventex/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+import eventex.core.views
+
+
+urlpatterns = [
+    path("", eventex.core.views.home),
+    path("admin/", admin.site.urls),
+]
